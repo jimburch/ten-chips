4ac5bc71d568a881d9135e6db9761bd04ce361fd
Jim Burch <jrburch1@gmail.com>
2024-10-11 10:12:30 -0700

    vite-setup

--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-import Picker from "./Picker/Picker"
-import Input from "./Input/Input"
-import Chips from "./Chips/Chips"
-
-export { Picker, Input, Chips }
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
-export const ResponseSchema = z.object({
-  answer: z.boolean(),
-  response: z.string(),
-})
-
-export type ResponseType = z.infer<typeof ResponseSchema>
